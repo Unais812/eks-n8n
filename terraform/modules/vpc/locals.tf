964da9568b680c385acbb4e5ada9731c31a02ce3
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-29 00:29:17 +0100

    configured blocks

--- terraform/modules/vpc/locals.tf
+++ terraform/modules/vpc/locals.tf
@@ -1,7 +1,9 @@
 locals {
   subnets = {
     "public-1"  = { cidr = "10.0.1.0/24", az = "eu-north-1a", public = true  }
-    "private-1" = { cidr = "10.0.3.0/24", az = "eu-north-1a", public = false }
+    "private-1" = { cidr = "10.0.2.0/24", az = "eu-north-1a", public = false }
+    "public-1"  = { cidr = "10.0.3.0/24", az = "eu-north-1b", public = true  }
+    "private-1" = { cidr = "10.0.4.0/24", az = "eu-north-1b", public = false }
     }   
 }
 
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -10,9 +10,6 @@ output "private_subnet_ids" {
   ]
 }
 
-# loops through the locals block and determines wether it is a private subnet or public,
-# it also outputs the id for them so i can reference in another module 
-
 output "public_subnet_ids" {
   value = [
     for k, subnet in aws_subnet.subnets :
