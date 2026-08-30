43a40b795f46dfb837d982f3f87a035a67515751
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-30 12:26:18 +0100

    added variables

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -15,3 +15,33 @@ variable "public_cidr" {
   type = string
   default = "0.0.0.0/0"
 }
+
+variable "public-subnet-1-cidr" {
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "private-subnet-1-cidr" {
+  type = string
+  default = "10.0.2.0/24"
+}
+
+variable "public-subnet-2-cidr" {
+  type = string
+  default = "10.0.3.0/24"
+}
+
+variable "private-subnet-2-cidr" {
+  type = string
+  default = "10.0.4.0/24"
+}
+
+variable "az1" {
+  type = string
+  default = "eu-north-1a"
+}
+
+variable "az2" {
+  type = string
+  default = "eu-north-1b"
+}
