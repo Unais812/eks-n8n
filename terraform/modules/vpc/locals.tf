c783b2a698782c9361cb6669dd4a0da33c908520
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-30 12:25:16 +0100

    removed the locals block for subnets

--- terraform/modules/vpc/locals.tf
+++ terraform/modules/vpc/locals.tf
@@ -1,12 +1,3 @@
 locals {
-  subnets = {
-    "public-1"  = { cidr = "10.0.1.0/24", az = "eu-north-1a", public = true  }
-    "private-1" = { cidr = "10.0.2.0/24", az = "eu-north-1a", public = false }
-    "public-1"  = { cidr = "10.0.3.0/24", az = "eu-north-1b", public = true  }
-    "private-1" = { cidr = "10.0.4.0/24", az = "eu-north-1b", public = false }
-    }   
-}
-
-locals {
-    name = "ECSv3"
+    name = "n8n"
 }
