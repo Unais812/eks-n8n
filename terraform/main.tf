e3b71ecddbb10d1722cf4752f59dcdd6791362cd
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-30 12:29:38 +0100

    changed variable from subnets to individual subnet ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,15 @@ module "eks" {
   source = "./modules/eks"
   vpc-id = module.vpc.vpc_id
   eks-node-group-role = module.iam.eks-node-group-role
-  private-subnets = module.vpc.private_subnet_ids
   vpc-cni-role = module.iam.vpc-cni-role
+  eks-cluster-role = module.iam.eks-cluster-role
+  eks-role-attachment = module.iam.eks-role-attachment
+  node-group-ec2-attachment = module.iam.node-group-ec2-attachment
+  node-group-ecr-attachment = module.iam.node-group-ecr-attachment
+  private-subnet-1 = module.vpc.private-subnet-1
+  private-subnet-2 = module.vpc.private-subnet-2
+  ebs_csi_driver_role_arn     = module.iam.ebs_csi_driver_role_arn
+
 }
 
 module "vpc" {
